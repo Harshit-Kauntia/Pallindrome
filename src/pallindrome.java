5d7442dff17d60a49cf7ef875be124c2b5cfc8f9
Harshit-Kauntia <hk2015@srmist.edu.in>
2026-02-17 10:34:15 +0530

    Executed UC2

--- src/pallindrome.java
+++ src/pallindrome.java
@@ -1,4 +1,22 @@
 package PACKAGE_NAME;
 
 public class pallindrome {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Pallindrome Checker App\n");
+
+        String ip = "madaam";
+        boolean isPallindrome = true;
+        for(int i=0; i<ip.length()/2; i++) {
+            if(ip.charAt(i) != ip.charAt(ip.length()-i-1)) {
+                isPallindrome = false;
+            }
+        }
+
+        if(isPallindrome) {
+            System.out.println("The input is a pallindrome!");
+        } else {
+            System.out.println("The input is NOT pallindorme!");
+        }
+    }
 }
+
