8f3e9a2b4110d7995b41f85135fce1728c41856b
Harshit-Kauntia <hk2015@srmist.edu.in>
2026-02-17 10:37:58 +0530

    Executed UC3

--- src/pallindrome.java
+++ src/pallindrome.java
@@ -1,4 +1,18 @@
 package PACKAGE_NAME;
 
 public class pallindrome {
-}
+        public static void main(String[] args) {
+            String ip = "madam";
+            String r_ip = "";
+            for(int i=ip.length()-1; i>=0; i--) {
+                r_ip += String.valueOf(ip.charAt(i));
+            }
+
+            if(ip.equals(r_ip)) {
+                System.out.println("ip string is pallindrome");
+            } else {
+                System.out.println("ip string is not a pallindrome");
+            }
+        }
+    }
+
