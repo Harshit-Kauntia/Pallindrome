28404d7c97f52e75b58a0682b94facb02d09bb49
Harshit-Kauntia <hk2015@srmist.edu.in>
2026-02-17 09:43:30 +0530

    Initialized

---
+++ src/PallindromeChecker.java
@@ -0,0 +1,9 @@
+public class PallindromeChecker {
+    public static void main(String args[])
+    {
+        System.out.println("Welcome to Pallindrome Checker System");
+        System.out.println("Version: 1.0");
+        System.out.println("System Initialized Successfully");
+
+    }
+}
